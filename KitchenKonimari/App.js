6f8bdfcf6ab1f65483e69bb2a0d4dcb79d627f03
Hassan Abbas <hassan.abbas@cgi.com>
2019-11-16 23:08:58 +0200

    Added new repo folder

---
+++ KitchenKonimari/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView
+        source={{ uri: 'http://localhost:3000' }}
+        style={{ marginTop: 20 }}
+      />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
