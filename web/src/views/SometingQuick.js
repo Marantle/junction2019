b69ed68f47252997547e13569fd91e989a8177d8
makkelip <markus.pikkanen@gmail.com>
2019-11-16 23:40:20 +0200

    display selected

--- web/src/views/SometingQuick.js
+++ web/src/views/SometingQuick.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Button, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
+import { Box, Button, GridList, GridListTile, GridListTileBar, Icon } from '@material-ui/core';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
 import { changeView, VIEW } from '../store/view';
 import { toggleSelectedProduct } from '../store/purchaseHistory';
 
 export default function LandingPage() {
   const someProducts = useSelector(state => Array.from(state.purchaseHistory.products).reverse().slice(0, 4));
+  const selectedProducts = useSelector(state => state.purchaseHistory.selectedProducts);
   const dispatch = useDispatch();
-  const onClick = (product) => {
+  const onProductSelect = (product) => {
     dispatch(toggleSelectedProduct(product));
   }
+  const onNext = () => {
+    dispatch(changeView(VIEW.RECIPE_SELECTION));
+  }
+
+  const isSelected = (product) => selectedProducts.some(p => p.ean === product.ean);
 
   return (
-    <Box display='flex' flexDirection='column' alignItems='center'>
+    <Box display='flex' flexDirection='column' alignItems='center' marginLeft='0.5em' marginRight='0.5em'>
       <p>We think you still have these, 
       which sounds the best right now?</p>
-      <GridList cellHeight={180} style={{ width: 500, height: 400 }}>
+      <GridList cellHeight={180} cols={2} >
         {someProducts.map(product => (
-          <GridListTile key={product.ean} onClick={() => onClick(product)} style={{ mouse: ''}}>
+          <GridListTile key={product.ean} onClick={() => onProductSelect(product)} style={{ mouse: ''}}>
               <img key={product.ean} src={product.pictureUrls[0].original} alt={product.ean} />
               <GridListTileBar
                 title={product.labelName.english}
+                actionIcon={isSelected(product) && <CheckCircleIcon style={{ color: '#FFF', margin: '0.2em'}}/>}
               />
           </GridListTile>
         ))}
       </GridList>
+      <Button
+        style={{ marginBottom: '1em' }}
+        size='large'
+        variant='contained'
+        color='secondary'
+        onClick={onNext}
+      >
+        Next
+      </Button>
     </Box>
   )
 }
